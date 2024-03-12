c4d6e32b654649b3e1ee113828b89c31823f3d8f
BrokenDreamStar <shuqiwang179@gmail.com>
2024-03-12 10:50:59 +0800

    updata

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import SwHome from '@/views/SwHome.vue'
-import SwAbout from '@/views/SwAbout.vue'
-import SwDownloads from '@/views/SwDownloads.vue'
-import SwGenshin from '@/views/SwGenshin.vue'
-import SwDt from '@/views/SwDt.vue'
-import SwMC from '@/views/SwMC.vue';
-import TRCInteriorSur from '@/views/TRCInteriorSur.vue'
-import JumpTRC from '@/views/JumpTRC.vue'
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
+  //使用History路由模式
   history: createWebHistory(import.meta.env.BASE_URL),
+  //切换页面不保存页面位置 始终位于顶端
   scrollBehavior(to, from, savePosition) {
     if (savePosition) {
       return savePosition
@@ -17,53 +11,54 @@ const router = createRouter({
       return { top: 0 }
     }
   },
+  //路由配置 使用动态路由
   routes: [
     {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      component: () => import('@/views/NotFound/404.vue')
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("@/views/NotFound/404.vue"),
     },
     {
-      path: '/',
-      name: 'home',
-      component: SwHome
+      path: "/",
+      name: "home",
+      component: () => import("@/views/SwHome.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: SwAbout
+      path: "/about",
+      name: "about",
+      component: () => import("@/views/SwAbout.vue"),
     },
     {
-      path: '/trcinteriorsur',
-      name: 'trcinteriorsur',
-      component: TRCInteriorSur
+      path: "/trcinteriorsur",
+      name: "trcinteriorsur",
+      component: () => import("@/views/TRCInteriorSur.vue"),
     },
     {
-      path: '/downloads',
-      name: 'downloads',
-      component: SwDownloads
+      path: "/downloads",
+      name: "downloads",
+      component: () => import("@/views/SwDownloads.vue"),
     },
     {
-      path: '/genshin',
-      name: 'genshin',
-      component: SwGenshin
+      path: "/genshin",
+      name: "genshin",
+      component: () => import("@/views/SwGenshin.vue"),
     },
     {
-      path: '/dt',
-      name: 'dt',
-      component: SwDt
+      path: "/dt",
+      name: "dt",
+      component: () => import("@/views/SwDt.vue"),
     },
     {
-      path: '/downloads/mc/:version',
-      name: 'SwMC',
-      component: SwMC
+      path: "/downloads/mc/:version",
+      name: "SwMC",
+      component: () => import("@/views/SwMC.vue"),
     },
     {
-      path: '/jumplink',
-      name: 'jumplink',
-      component: JumpTRC
-    }
-  ]
+      path: "/jumplink",
+      name: "jumplink",
+      component: () => import("@/views/JumpTRC.vue"),
+    },
+  ],
 })
 
 export default router
--- src/views/SwMC.vue
+++ src/views/SwMC.vue
@@ -1,11 +1,42 @@
 <script setup>
+import SwNav from '@/components/SwNav.vue'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+
+const route = useRoute()
+const downloadPageData = ref([])
+
+onMounted(async () => {
+  try {
+    const storedData = sessionStorage.getItem('downloadPageData')
+    if (storedData) {
+      downloadPageData.value = JSON.parse(storedData)
+    } else {
+      const res = await axios(`http://starm.team:3000/${route.params.version}`)
+      downloadPageData.value = res.data
+      sessionStorage.setItem('downloadPageData', JSON.stringify(res.data))
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 </script>
 
 <template>
+  <SwNav></SwNav>
+  <div class="top-box"></div>
   <div>
     {{ $route.params.version }}
+    <ul>
+      <li v-for="item in downloadPageData" :key="item.id">{{ item.version }}</li>
+    </ul>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.top-box {
+  height: 50px;
+}
+</style>
