606667172da50b6520a1d504c45f811ac971c034
BrokenDreamStar <shuqiwang179@gmail.com>
2024-03-29 16:47:00 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from "./App.vue"
 import router from "./router"
 import axios from "axios"
 
-axios.defaults.baseURL = "http://127.0.0.1:3000"
+axios.defaults.baseURL = "https://starm.team:3000"
 
 const app = createApp(App)
 
