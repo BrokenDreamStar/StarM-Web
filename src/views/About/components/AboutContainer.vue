fcfd2514cdac1c2cf7f4615df2712bd12930a126
BrokenDreamStar <shuqiwang179@gmail.com>
2024-04-11 16:26:14 +0800

    拆分为模块化

---
+++ src/views/About/components/AboutContainer.vue
@@ -0,0 +1,78 @@
+<script setup>
+defineProps({
+	imgName: {
+		type: String,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	}
+})
+</script>
+
+<template>
+	<div class="container">
+		<div class="title">
+			<div>
+				<img :src="`/src/assets/icon/${imgName}`" alt="" />
+				<span>{{ title }}</span>
+			</div>
+		</div>
+		<el-row>
+			<slot></slot>
+		</el-row>
+	</div>
+</template>
+
+<style lang="less" scoped>
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: relative;
+	width: 100%;
+	height: fit-content;
+	margin-top: 2.5rem;
+	background-color: #fff;
+	border-radius: 0.5rem;
+	box-shadow: rgba(0, 0, 0, 0.1) 0rem 0.625rem 3.125rem;
+	.title {
+		position: absolute;
+		top: -1.25rem;
+		left: 1.25rem;
+		width: fit-content;
+		min-width: 3.125rem;
+		height: 2.5rem;
+		background-color: #00b3ff;
+		border-radius: 0.5rem;
+		box-shadow: rgba(0, 0, 0, 0.1) 0 0.3125rem 0.5rem;
+		-webkit-user-select: none;
+		user-select: none;
+
+		div {
+			margin: 0 0.625rem 0 0.625rem;
+
+			span {
+				color: #fff;
+				line-height: 2.5rem;
+				text-align: center;
+				font-weight: 600;
+			}
+
+			img {
+				position: relative;
+				top: -1px;
+				width: 2rem;
+				height: 2rem;
+				vertical-align: middle;
+				margin-right: 0.1875rem;
+			}
+		}
+	}
+	.el-row {
+		width: 100%;
+		margin: 1.5625rem 0 1.5625rem 0;
+	}
+}
+</style>
---
+++ src/views/About/components/TechAndTools.vue
@@ -0,0 +1,83 @@
+<script setup>
+defineProps({
+	data: {
+		type: Object,
+		required: true
+	}
+})
+
+const imgUrl = (url) => {
+	return new URL(`/src/assets/icon/logo/${url}`, import.meta.url)
+}
+</script>
+
+<template>
+	<el-col
+		v-for="item in data"
+		:key="item.id"
+		:xs="12"
+		:sm="12"
+		:md="8"
+		:lg="6"
+		:xl="4"
+	>
+		<a :href="item.link" target="_blank">
+			<div class="logo">
+				<img :src="imgUrl(item.img_name)" alt="" />
+			</div>
+			<div class="text">
+				<span class="title">{{ item.title }}</span>
+				<span class="description">{{ item.description }}</span>
+			</div>
+		</a>
+	</el-col>
+</template>
+
+<style lang="less" scoped>
+.el-col {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0.9375rem 0;
+
+	a {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		min-width: 11.25rem;
+		height: 3.125rem;
+		cursor: pointer;
+
+		.logo {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 3.125rem;
+			height: 3.125rem;
+			margin-right: 0.1875rem;
+
+			img {
+				width: 45px;
+				height: 45px;
+			}
+		}
+
+		.text {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			justify-content: center;
+
+			.title {
+				font-size: 1.0625rem;
+				font-weight: 600;
+			}
+
+			.description {
+				font-size: 0.9375rem;
+				color: rgb(127, 129, 132);
+			}
+		}
+	}
+}
+</style>
